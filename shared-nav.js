80f121314a39cd015d9df9cb28a345c2e2167a87
TheAlbrvr <the09alby@gmail.com>
2026-07-10 14:22:05 +0200

    Modify footer with privacy and cookie policy links
    Updated footer content to include privacy and cookie policy links.

--- shared-nav.js
+++ shared-nav.js
@@ -74,8 +74,12 @@ const footer = `
   </div>
 
   <div class="footer-bottom">
-    <p>© 2026 ACADEMY Evergreen Burger. Tutti i diritti riservati.
-  developed by: <a href="https://www.instagram.com/alberto.vegro?igsh=N2Jhb2xiYXM1enQ5">Vegro Alberto</a> </p>
+    <p>© 2026 ACADEMY Evergreen Burger. by <a href="https://www.instagram.com/alberto.vegro?igsh=N2Jhb2xiYXM1enQ5">Vegro Alberto</a> Tutti i diritti riservati.
+   <br>
+   <a href="https://www.iubenda.com/privacy-policy/34022629" class="iubenda-white iubenda-noiframe iubenda-embed" title="Privacy Policy ">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+   <br>
+   <a href="https://www.iubenda.com/privacy-policy/34022629/cookie-policy" class="iubenda-white iubenda-noiframe iubenda-embed" title="Cookie Policy ">Cookie Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+   </p>
   </div>
 </footer>`;
 
